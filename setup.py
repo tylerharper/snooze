8d9264c80c8f2740a406cc62cb106147dced57ae
knobe <knobe.sterling@gmail.com>
2009-11-21 08:10:15 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='snooze',
+      version='0.0.1',
+      description='A generic RESTful API generator',
+      author='Tyler Harper',
+      url='http://github.com/knobe/snooze',
+      packages=['snooze'],
+      license='MIT'
+     )
