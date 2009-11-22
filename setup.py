010259ee0b34843529b66d3af3c3b2e0ce122632
Tyler Harper <knobe.sterling@gmail.com>
2009-11-21 19:46:39 -0800

    Now we use setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
 
 setup(name='snooze',
-      version='0.0.1',
-      description='A generic RESTful API generator',
-      author='Tyler Harper',
-      url='http://github.com/knobe/snooze',
-      packages=['snooze'],
-      license='MIT'
+      version=version,
+      author_email='tyler@phrick.org',
+      url='http://phrick.org/projects',
+      license='MIT License',
+      include_package_date=True,
+      zip_safe=True,
      )
